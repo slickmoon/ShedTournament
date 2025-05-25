7f9040797fde25161eb841baaa384444be959e52
Nicholas Cass <nickcass88@gmail.com>
2025-05-25 12:50:08 +1000

    Accordionize K/D ratios screen for space

--- frontend/src/components/PlayerKD.tsx
+++ frontend/src/components/PlayerKD.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography, Paper } from '@mui/material';
+import { Box, Typography, Paper, Accordion, AccordionSummary, AccordionDetails, ExpandMoreIcon } from '@mui/material';
 import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
 
 interface PlayerKD {
@@ -19,7 +19,9 @@ const PlayerKDs: React.FC<PlayerKDProps> = ({ playerKd }) => {
     <Box sx={{ flex: 1 }}>
       <h2>Current K/D Ratios</h2>
       <Paper elevation={3} sx={{ p: 2, maxWidth: 400, mx: 'auto' }}>
-        {playerKd.map((kdplayer) => (
+        {[...playerKd]
+          .slice(0, 3)
+          .map((kdplayer) => (
           <Box
             key={kdplayer.player_id}
             sx={{
@@ -51,6 +53,49 @@ const PlayerKDs: React.FC<PlayerKDProps> = ({ playerKd }) => {
               </Typography>
             </Box>
           </Box>
+        ))}
+        {[...playerKd]
+          .slice(3)
+          .map((kdplayer) => (
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography>Show more</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Box
+                key={kdplayer.player_id}
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: 2,
+                  p: 1,
+                  mb: 1,
+                  borderRadius: 1,
+                  '&:hover': {
+                    backgroundColor: 'action.hover'
+                  }
+                }}
+              >
+                
+                <Box sx={{ flex: 1 }}>
+                  <Typography variant="body1">
+                    <Typography component="span" variant="body1" sx={{ fontWeight: 'bold' }}>
+                      {kdplayer.player_name}
+                    </Typography>
+                    : {kdplayer.kd} (
+                    <Typography component="span" sx={{ color: 'success.main' }}>
+                      {kdplayer.wins}
+                    </Typography>
+                    /
+                    <Typography component="span" sx={{ color: 'error.main' }}>
+                      {kdplayer.losses}
+                    </Typography>
+                    )
+                  </Typography>
+                </Box>
+              </Box>
+            </AccordionDetails>
+          </Accordion>
         ))}
       </Paper>
     </Box>
