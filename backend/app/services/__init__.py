7bc9ca9a9422e44f524b35a60df67ed9a496aa91
Nicholas Cass <nickcass88@gmail.com>
2025-05-26 21:20:49 +1000

    Fix init

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,5 +1,6 @@
 from .player_service import PlayerService
 from .match_service import MatchService
 from .audit_log_service import AuditLogService
+from .stats_service import StatsService
 
-__all__ = ['PlayerService', 'MatchService', 'AuditLogService'] 
+__all__ = ['PlayerService', 'MatchService', 'AuditLogService', 'StatsService'] 
