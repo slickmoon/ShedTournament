0809903bb08f892c3717fca8c7bbb9ec9f0b9e10
Nicholas Cass <nickcass88@gmail.com>
2025-06-24 01:27:15 +1000

    Add token verification for seasons endpoint Added response model for get seasons endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -251,7 +251,10 @@ async def get_most_matches_in_day(
 ):
     return StatsService.get_most_matches_in_day(db)
 
-@app.get("/seasons")
-async def get_seasons(db: Session = Depends(database.get_db)):
+@app.get("/seasons", response_model=list[dict])
+async def get_seasons(
+    db: Session = Depends(database.get_db),
+    token: dict = Depends(verify_token)
+):
     return PlayerService.get_seasons(db)
 
